25a1146ff7d9e34b42a72e64463c6904b8a7d620
Psilocybe28 <0juanmanueldominguezlopez@gmail.com>
2022-12-01 12:02:15 -0600

    edad

---
+++ Tipos_de_datos/beca.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingresa tu edad "))
+if edad >= 18 and edad <= 25:
+    print("Dar bequita")
+else:
+    print("Gracias por participar")
---
+++ Tipos_de_datos/puntos.py
@@ -0,0 +1,9 @@
+puntos = float(input("Ingresa puntos "))
+if puntos >= 0 and puntos <=5:
+    print("C")
+elif puntos > 5 and puntos <=7:
+    print("B")
+elif puntos > 7 and puntos <=10:
+    print("A")
+else:
+    print("No valido")
